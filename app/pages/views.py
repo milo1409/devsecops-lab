c4ce783e9b0ca7b328692f212738900f6a00455c
milo1409 <milo1409@gmail.com>
2026-03-08 14:15:06 -0700

    Prueba Snyk

--- app/pages/views.py
+++ app/pages/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html", {
-        "title": "Demo DevSecOps Prueba",
+        "title": "Demo DevSecOps Prueba Numero 2",
         "msg": "Django + CI/CD (GitHub Actions + Jenkins) + Sonar/Snyk + Prometheus/Grafana"
     })
